d7838230eef7c3df318a6dd63372f9eee4bfdbda
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-10 16:43:25 +0400

    add MongoDb

---
+++ src/db/connectMongo.ts
@@ -0,0 +1,22 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://SaninaUlyana:${process.env.MONGODB_PASSWORD}@collections.zfhzge0.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const run = async() => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
