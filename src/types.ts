2382e36294dbe305e6d0602b20a2656e9ac653d8
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-10 13:07:50 +0400

    initial types

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+export interface ICollection {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  image?: string;
+}
+
+export interface IItem {
+  id: string;
+  collectionId: string;
+  name: string;
+  tags: string[];
+}
+
+export enum CollectibleType {
+  Coins = "Coins",
+  EnvelopesAndStamps = "Envelopes and Stamps",
+  Postcards = "Postcards",
+  Books = "Books",
+  Photographs = "Photographs",
+  PaintingsAndArt = "Paintings and Art",
+  PorcelainFigurines = "Porcelain Figurines",
+  ModelCars = "Model Cars",
+  Dolls = "Dolls",
+  SportsCards = "Sports Cards",
+  VinylRecords = "Vinyl Records",
+  EventTickets = "Event Tickets",
+  FootballScarves = "Football Scarves",
+  NumismaticSets = "Numismatic Sets",
+  VintageClothing = "Vintage Clothing",
+  Badges = "Badges",
+  Watches = "Watches",
+  AntiqueToys = "Antique Toys",
+  PerfumeBottles = "Perfume Bottles",
+  Birdhouses = "Birdhouses",
+  CupsAndTeaSets = "Cups and Tea Sets",
+  Stickers = "Stickers",
+  PorcelainDishware = "Porcelain Dishware",
+  GemstonesAndMinerals = "Gemstones and Minerals",
+  MoviePosters = "Movie Posters",
+  CollectibleKnives = "Collectible Knives",
+  Maps = "Maps",
+  Banknotes = "Banknotes",
+  MilitaryMemorabilia = "Military Memorabilia",
+  Magnets = "Magnets",
+}
