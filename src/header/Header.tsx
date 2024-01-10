2bfe58469190f911a67224615ee04cc8ccf8276f
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-10 16:41:17 +0400

    add MainLayout container in header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,15 +1,20 @@
-import { Flex, Heading, Link } from "@chakra-ui/react";
+import MainLayout from "@/main-layout";
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-      <Flex direction="row" alignItems="center" gap="5" bg="green.500">
-        <Link href="/">
-          <Heading as="h1" size="2xl">
-            Coll
-          </Heading>
-        </Link>
-        <Link href="collection-list-page">Collections</Link>
-      </Flex>
+    <Box bg="green.500">
+      <MainLayout>
+        <Flex direction="row" alignItems="center" gap="5" mb="10px">
+          <Link href="/">
+            <Heading as="h1" size="2xl">
+              Coll
+            </Heading>
+          </Link>
+          <Link href="collection-list-page">Collections</Link>
+        </Flex>
+      </MainLayout>
+    </Box>
   );
 };
 
