21765e455d36b5ba0d1b82e385c6d0f52ada2260
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-10 16:43:06 +0400

    add Link for transition in separate Item component & delete "delete-button"

--- src/item-list/ItemList.tsx
+++ src/item-list/ItemList.tsx
@@ -1,5 +1,4 @@
 import { mockDataItems } from "@/mockDataItems";
-import { DeleteIcon } from "@chakra-ui/icons";
 import {
   Card,
   Stack,
@@ -7,9 +6,9 @@ import {
   Heading,
   CardFooter,
   Text,
-  IconButton,
   Tag,
   Flex,
+  Link,
 } from "@chakra-ui/react";
 
 const ItemList = () => {
@@ -17,27 +16,24 @@ const ItemList = () => {
     <Flex direction="column" gap={2}>
       {mockDataItems.map((item) => (
         <Card overflow="hidden" variant="outline" maxH="200px" key={item.id}>
-          <Stack direction="row" alignItems="center" flex="1">
-            <IconButton
-              aria-label="Delete Item"
-              icon={<DeleteIcon />}
-              ml="20px"
-            />
-            <CardBody>
-              <Heading size="md">{item.name}</Heading>
-              <Text py="2">
-                Caffè latte is a coffee beverage of Italian origin made with
-                espresso and steamed milk.
-              </Text>
-            </CardBody>
-            <CardFooter display="flex" flexWrap="wrap" maxW="500px">
-              {item.tags.map((tag, index) => (
-                <Tag marginRight="2" marginBottom="2" key={index}>
-                  {tag}
-                </Tag>
-              ))}
-            </CardFooter>
-          </Stack>
+          <Link href="item-page">
+            <Stack direction="row" alignItems="center" flex="1">
+              <CardBody>
+                <Heading size="md">{item.name}</Heading>
+                <Text py="2">
+                  Caffè latte is a coffee beverage of Italian origin made with
+                  espresso and steamed milk.
+                </Text>
+              </CardBody>
+              <CardFooter display="flex" flexWrap="wrap" maxW="500px">
+                {item.tags.map((tag, index) => (
+                  <Tag marginRight="2" marginBottom="2" key={index}>
+                    {tag}
+                  </Tag>
+                ))}
+              </CardFooter>
+            </Stack>
+          </Link>
         </Card>
       ))}
     </Flex>
