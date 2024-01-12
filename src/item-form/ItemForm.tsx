ab4b7edaae3de6a55760007055bfb9571dee0a4f
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-12 13:15:09 +0400

    add ItemForm component

---
+++ src/item-form/ItemForm.tsx
@@ -0,0 +1,43 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input
+} from "@chakra-ui/react";
+
+const ItemForm = () => {
+  return (
+    <Box>
+      <Heading size="md" mb="20px">
+        Add an item
+      </Heading>
+
+      <FormControl mb="40px">
+        <FormLabel mb="2px">Name:</FormLabel>
+        <Input variant="flushed" />
+      </FormControl>
+
+      <FormControl mb="40px">
+        <FormLabel mb="2px">Description:</FormLabel>
+        <Input variant="flushed" />
+      </FormControl>
+
+      {/* <FormControl>
+        <FormLabel mb="8px">Tags:</FormLabel>
+        <Input variant="flushed" />
+      </FormControl>
+      
+      <Tag>
+        {tag}
+      </Tag> */}
+
+      <Button colorScheme="teal" size="lg">
+        Add
+      </Button>
+    </Box>
+  );
+};
+
+export default ItemForm;
