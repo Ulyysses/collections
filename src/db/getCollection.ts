b6b37c229c7d31fd390512e3992ed855d774ad9a
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-16 13:13:01 +0400

    fix function of getting collection

--- src/db/getCollection.ts
+++ src/db/getCollection.ts
@@ -1,18 +1,24 @@
 "use server";
 
+import { ObjectId } from "mongodb";
 import { client } from "./connectMongo";
 
 export const getCollection = async (id: string) => {
+  let result
   try {
     await client.connect();
     const database = client.db("collections");
     const collection = database.collection("collection_list");
 
-    return await collection.findOne({}, { _id: id });
+    const objectId = new ObjectId(id);
+    result = await collection.findOne({ _id: objectId });
+    
   } catch (error) {
     console.log(error);
     throw error;
   } finally {
     await client.close();
   }
+
+  return result;
 };
