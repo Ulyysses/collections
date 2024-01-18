f5dffdef92ce9128ac82aa35ca914e34aabaf05c
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-18 12:54:05 +0400

    rewrite form with react-hook-form

--- src/item-form/ItemForm.tsx
+++ src/item-form/ItemForm.tsx
@@ -15,66 +15,50 @@ import {
   InputRightElement,
   Tag,
 } from "@chakra-ui/react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface ItemFormProps {
   collectionId: string;
 }
 
 const ItemForm = ({ collectionId }: ItemFormProps) => {
-  const [value, setValue] = useState<IItem>({
-    collectionId: collectionId,
-    name: "",
-    tagsId: [],
+  const { register, handleSubmit, reset } = useForm<IItem>({
+    defaultValues: {
+      collectionId: collectionId,
+      name: "",
+      tagsId: [],
+    },
   });
+
   const [tagValue, setTagValue] = useState("");
   const [tags, setTags] = useState<string[]>([]);
 
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue({
-      ...value,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleAddNewTag = () => {
-    setTags((prevTags) => [...prevTags, tagValue]);
-    setTagValue("");
-  };
-
-  useEffect(() => {
-    const tagData = async () => {
-      try {
-        const tagPromises = tags.map((tag) => addNewTag(tag));
-        const savedTags = await Promise.all(tagPromises);
-        const tagIds = savedTags.map((savedTag) => savedTag._id);
-
-        setValue((prevValue) => ({
-          ...prevValue,
-          tagsId: [...tagIds],
-        }));
-        
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    tagData();
-  }, [tags]);
-
-  const handleAddNewItem = async () => {
+  const onSubmit = async (data: IItem) => {
     try {
-      await addNewItem(value);
+      const tagPromises = tags.map((tag) => addNewTag(tag));
+      const savedTags = await Promise.all(tagPromises);
+      const tagIds = savedTags.map((savedTag) => savedTag._id);
 
-      setValue({
+      data.tagsId = tagIds;
+
+      await addNewItem(data);
+
+      reset({
         collectionId: "",
         name: "",
         tagsId: [],
       });
       setTags([]);
+      setTagValue("")
     } catch (error) {
       console.error("Error adding new item:", error);
     }
+  };
+
+  const handleAddNewTag = () => {
+    setTags((prevTags) => [...prevTags, tagValue]);
+    setTagValue("");
   };
 
   return (
@@ -83,45 +67,49 @@ const ItemForm = ({ collectionId }: ItemFormProps) => {
         New Item
       </Heading>
 
-      <FormControl mb="40px">
-        <FormLabel mb="2px">Name:</FormLabel>
-        <Input
-          variant="flushed"
-          type="text"
-          name="name"
-          value={value.name}
-          onChange={onChange}
-        />
-      </FormControl>
+<form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl mb="40px">
+          <FormLabel mb="2px">Name:</FormLabel>
+          <Input
+          {...register("name", { required: true })}
+            variant="flushed"
+            type="text"
+          />
+        </FormControl>
+        <InputGroup size="md" mb="20px">
+          <FormLabel mb="8px">Tags:</FormLabel>
+          <Input
+            variant="flushed"
+            pr="4.5rem"
+            type="text"
+            name="tagValue"
+            value={tagValue}
+            onChange={(e) => setTagValue(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleAddNewTag}>
+              Add tag
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        
+        <Box mb="40px">
+          {tags.map((tag, index) => (
+            <Tag marginRight="2" marginBottom="2" key={index}>
+              {tag}
+            </Tag>
+          ))}
+        </Box>
 
-      <InputGroup size="md" mb="20px">
-        <FormLabel mb="8px">Tags:</FormLabel>
-        <Input
-          variant="flushed"
-          pr="4.5rem"
-          type="text"
-          name="tagValue"
-          value={tagValue}
-          onChange={(e) => setTagValue(e.target.value)}
-        />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleAddNewTag}>
-            Add tag
-          </Button>
-        </InputRightElement>
-      </InputGroup>
-
-      <Box mb="40px">
-        {tags.map((tag, index) => (
-          <Tag marginRight="2" marginBottom="2" key={index}>
-            {tag}
-          </Tag>
-        ))}
-      </Box>
-
-      <Button colorScheme="teal" size="lg" w="84px" onClick={handleAddNewItem}>
-        Add
-      </Button>
+        <Button
+          colorScheme="teal"
+          size="lg"
+          w="84px"
+          type="submit"
+        >
+          Add
+        </Button>
+        </form>
     </Flex>
   );
 };
