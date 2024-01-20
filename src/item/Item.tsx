b2dfcb87c77ef6c213938849b9429e1cf8f5ad1b
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-20 21:35:57 +0400

    render current item on separate page

--- src/item/Item.tsx
+++ src/item/Item.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { deleteItem } from "@/db/deletion/deleteItem";
+import { getItem } from "@/db/receiving/getItem";
+import { getTag } from "@/db/receiving/getTag";
+import Loader from "@/loader";
+import { IItem } from "@/types";
+import WarningModal from "@/warning-modal";
 import {
   Button,
   Card,
@@ -7,46 +15,108 @@ import {
   Stack,
   Image,
   Text,
+  Tag,
+  useDisclosure,
+  Box,
 } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface CollectionProps {
   id: string;
 }
 
 const Item = ({ id }: CollectionProps) => {
-  return (
-    <Card
-      direction={{ base: "column", sm: "row" }}
-      overflow="hidden"
-      variant="outline"
-      minH="300px"
-    >
-      <Image
-        p="10px"
-        objectFit="cover"
-        maxW={{ base: "100%", sm: "200px" }}
-        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-        alt="Caffe Latte"
-      />
-      <Stack>
-        <CardBody>
-          <Heading size="md">The perfect latte</Heading>
+  const [item, setItem] = useState<IItem>();
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-          <Text py="2">
-            Caffè latte is a coffee beverage of Italian origin made with
-            espresso and steamed milk.
-          </Text>
-        </CardBody>
-        <CardFooter display="flex" gap="5px">
-          <Button variant="solid" colorScheme="blue">
-            Edit
-          </Button>
-          <Button variant="ghost" colorScheme="blue">
-            Delete
-          </Button>
-        </CardFooter>
-      </Stack>
-    </Card>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const receivedItem = await getItem(id);
+        setItem(receivedItem);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      if (item) {
+        try {
+          const tagNames = await renderTags(item?.tagsId);
+          setItem({ ...item, tagNames });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+    fetchTags();
+  }, [item]);
+
+  const renderTags = async (tagsId: string[]) => {
+    const tagNames = await Promise.all(tagsId.map((tagId) => getTag(tagId)));
+    return tagNames.filter((tagName) => tagName !== undefined) as string[];
+  };
+
+  const handleDeleteItem = async (id: string) => {
+    try {
+      await deleteItem(id);
+      router.back();
+    } catch (error) {
+      console.error("Error handling delete:", error);
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+        minH="300px"
+      >
+        <Image
+          p="10px"
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+          alt="Caffe Latte"
+        />
+        <Stack>
+          <CardBody display="flex" flexDirection="column" gap="20px">
+            <Heading size="md">{item?.name}</Heading>
+            {item?.description && <Text py="2">{item.description}</Text>}
+            <Box>
+              {item?.tagNames?.map((tagName, index) => (
+                <Tag marginRight="2" marginBottom="2" key={index}>
+                  {tagName}
+                </Tag>
+              ))}
+            </Box>
+          </CardBody>
+          <CardFooter display="flex" gap="5px">
+            <Button variant="solid" colorScheme="blue">
+              Edit
+            </Button>
+            <Button variant="ghost" colorScheme="blue" onClick={() => handleDeleteItem(id)}>
+              Delete
+            </Button>
+          </CardFooter>
+        </Stack>
+      </Card>
+      {/* <WarningModal isOpen={isOpen} onClose={onClose} id={id}/> */}
+    </>
   );
 };
 
