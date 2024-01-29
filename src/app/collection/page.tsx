d8ffb4c8eaf3ae7ac0e85b389788b48663ea9c08
Ulyana Sanina <sanina.ulyana111@gmail.com>
2024-01-29 15:31:52 +0400

    get data in server component instead of server actions

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,15 +1,19 @@
 import { Metadata } from "next";
 import MainLayout from "@/main-layout";
 import CollectionList from "@/collection-list";
+import { collection_list, connectionMongo } from "@/db/connectMongo";
 
 export const metadata: Metadata = {
   title: "Collection list",
 };
 
-export default function CollectionListPage() {
+export default async function CollectionListPage() {
+  await connectionMongo;
+  const collectionsData = JSON.parse(JSON.stringify(await collection_list.find({})));
+
   return (
     <MainLayout>
-      <CollectionList />
+      <CollectionList collectionList={collectionsData}/>
     </MainLayout>
   );
 }
--- src/collection-list/CollectionList.tsx
+++ src/collection-list/CollectionList.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { getCollectionList } from "@/db/receiving/getCollectionList";
-import Loader from "@/loader";
 import { ICollection } from "@/types";
 import {
   Card,
@@ -12,43 +10,8 @@ import {
   Flex,
   Link,
 } from "@chakra-ui/react";
-import { FlattenMaps } from "mongoose";
-import { useEffect, useState } from "react";
 
-const CollectionList = () => {
-  const [collectionList, setCollectionList] = useState<ICollection[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const listData: (FlattenMaps<any> & Required<{ _id: unknown }>)[] =
-          await getCollectionList();
-
-        const processedList: ICollection[] = listData.map((collection) => {
-          return {
-            _id: String(collection._id),
-            title: String(collection.title),
-            description: String(collection.description),
-            category: String(collection.category),
-          };
-        });
-
-        setCollectionList(processedList);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) {
-    return <Loader />;
-  }
-
+const CollectionList = ({ collectionList }: { collectionList: ICollection[] }) => {
   return (
     <Flex direction="column" gap={2}>
       {collectionList.map((collection) => (
